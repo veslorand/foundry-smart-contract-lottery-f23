53d8a4b10c8f84760409bc963f99777d5784c52f
veslorand <veslorand@gmail.com>
2024-02-09 21:36:29 +0100

    Initial commit

---
+++ src/Raffle.sol
@@ -0,0 +1,48 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A sample Raffle Contract
+ * @author Lorand Veres
+ * @notice This contract is for creating a sample raffle
+ * @dev Impolements Chainlink VRFv2
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /** Getter functions */
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
