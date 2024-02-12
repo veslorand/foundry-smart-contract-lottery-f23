2d5214b80594c6c422da8d83f8b2957f3b2aac39
veslorand <veslorand@gmail.com>
2024-02-12 17:32:16 +0100

    Reset players array and timestamp. Emit PickedWinner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -64,6 +64,7 @@ contract Raffle is VRFConsumerBaseV2 {
     /** Events */
 
     event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -119,6 +120,11 @@ contract Raffle is VRFConsumerBaseV2 {
         address payable winner = s_players[indexOfWinner];
         s_recentWinner = winner;
         s_raffleState = RaffleState.OPEN;
+
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
+
         (bool success, ) = winner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
