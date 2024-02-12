e999695737342e5005df512827add427b7b04f37
veslorand <veslorand@gmail.com>
2024-02-12 16:41:55 +0100

    Solve imports and override fulfillRandomWords

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 // Layout of Contract:
 // version
 // imports
@@ -19,26 +21,57 @@
 // private
 // view & pure functions
 
-// SPDX-Licence-Identifier: MIT
-
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "lib/forge-std/lib/chainlink-brownie-contracts-main/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "lib/forge-std/lib/chainlink-brownie-contracts-main/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+///home/veslorand/solidity/foundry-f23/foundry-smart-contract-lottery-f23/lib/forge-std/lib/chainlink-brownie-contracts-main/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol
 /**
  * @title A sample Raffle Contract
  * @author Lorand Veres
  * @notice This contract is for creating a sample raffle
  * @dev Impolements Chainlink VRFv2
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEthSent();
+
+    /** State Variables */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_gasLane;
+    VRFCoordinatorV2Interface private i_vfrCoordinator;
+    uint64 private i_subscriptionId;
+    uint32 i_callbackGasLimit;
+
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     /** Events */
+
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint256 requestConfirmations,
+        uint32 callbackGasLimit,
+        uint256 numWords
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_gasLane = gasLane;
+        i_vfrCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -48,10 +81,27 @@ contract Raffle {
         s_players.push(payable(msg.sender));
 
         emit EnteredRaffle(msg.sender);
-        
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert();
+        }
+        uint256 requestId = i_vfrCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        
+    }
 
     /** Getter functions */
 
